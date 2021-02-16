44bc246f99277cca07ad6939653eea273d37a80d
Florian Romijn <florian.romijn@gmail.com>
2021-02-16 17:59:22 +0100

    toevoeging softmax activation functie op de output layer.

--- NNFS_Enviornment/NNFS_Enviornment.py
+++ NNFS_Enviornment/NNFS_Enviornment.py
@@ -1,17 +1,10 @@
 import sys
 import numpy as np
-import matplotlib
+import matplotlib as plt
 import nnfs
 from nnfs.datasets import spiral_data
 
 nnfs.init()
-
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-
-X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -24,48 +17,24 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2, 5)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
 
+dense1.forward(X)
+activation1.forward(dense1.output)
 
-    
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
 
-
-
-
-'''
-layer_outputs = [] #output van de huidige laag
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-
-print(layer_outputs)
-
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2, 3, 0.5]
-
-
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, 0.73, -0.13]]
-
-biases = [2, 3, 0.5]
-
-biases2 = [-1, 2, -0.5]
-
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-print(layer2_outputs)
-
-'''
+print(activation2.output[:5])
